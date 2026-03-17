13fddec6ede948331b7c654d6b1644cc7f34c345
Mariya <maripolevay@gmail.com>
2026-03-17 19:04:17 +0500

    1.1

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,8 +1,32 @@
+function runNextTask(taskQueue) {
+    if (taskQueue.running || taskQueue.tasks.length === 0) {
+        return;
+    }
+    taskQueue.running = true;
+    const task = taskQueue.tasks.shift();
+
+    if (task.runAndContinue) {
+        setTimeout(() => {
+            task.runAndContinue(() => {
+                task.dispose && task.dispose();
+                taskQueue.running = false;
+
+                setTimeout(() => {
+                    runNextTask(taskQueue);
+                });
+            });
+        }, 0);
+    } else {
+        runNextTask(taskQueue);
+    }
+}
+
+
 class TaskQueue {
     tasks = [];
     running = false;
 
-    push(run, dispose, duration){
+    push(run, dispose, duration) {
         if (duration === undefined || duration === null) {
             this.tasks.push({runAndContinue: run, dispose});
         } else {
@@ -16,37 +40,12 @@ class TaskQueue {
                 dispose
             });
         }
-        this.runNextTask(this);
+        runNextTask(this);
     }
 
-    continueWith (action){
+    continueWith(action) {
         this.push(action, null, 0);
-    }
-
-    runNextTask(taskQueue) {
-        if (taskQueue.running || taskQueue.tasks.length === 0) {
-            return;
-        }
-        taskQueue.running = true;
-        const task = taskQueue.tasks.shift();
-
-        if (task.runAndContinue) {
-            setTimeout(() => {
-                task.runAndContinue(() => {
-                    task.dispose && task.dispose();
-                    taskQueue.running = false;
-
-                    setTimeout(() => {
-                        this.runNextTask(taskQueue);
-                    });
-                });
-            }, 0);
-        }
-        else {
-            this.runNextTask(taskQueue);
-        }
     }
 }
 
 export default TaskQueue;
-
