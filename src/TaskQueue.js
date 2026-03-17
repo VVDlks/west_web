59d14a6acaa0c1bcb7471a7a74b3f11ee056d3c2
Mariya <maripolevay@gmail.com>
2026-03-17 19:26:00 +0500

    1.1

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,27 +1,3 @@
-function runNextTask(taskQueue) {
-    if (taskQueue.running || taskQueue.tasks.length === 0) {
-        return;
-    }
-    taskQueue.running = true;
-    const task = taskQueue.tasks.shift();
-
-    if (task.runAndContinue) {
-        setTimeout(() => {
-            task.runAndContinue(() => {
-                task.dispose && task.dispose();
-                taskQueue.running = false;
-
-                setTimeout(() => {
-                    runNextTask(taskQueue);
-                });
-            });
-        }, 0);
-    } else {
-        runNextTask(taskQueue);
-    }
-}
-
-
 class TaskQueue {
     tasks = [];
     running = false;
@@ -49,3 +25,27 @@ class TaskQueue {
 }
 
 export default TaskQueue;
+
+
+function runNextTask(taskQueue) {
+    if (taskQueue.running || taskQueue.tasks.length === 0) {
+        return;
+    }
+    taskQueue.running = true;
+    const task = taskQueue.tasks.shift();
+
+    if (task.runAndContinue) {
+        setTimeout(() => {
+            task.runAndContinue(() => {
+                task.dispose && task.dispose();
+                taskQueue.running = false;
+
+                setTimeout(() => {
+                    runNextTask(taskQueue);
+                });
+            });
+        }, 0);
+    } else {
+        runNextTask(taskQueue);
+    }
+}
